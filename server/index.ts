4a10c1864995fbf20f4586792bd1eebf1cccab45
Han Wang <hyw2@cornell.edu>
2022-04-27 21:28:14 -0400

    Remove datadog

--- server/index.ts
+++ server/index.ts
@@ -11,19 +11,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const dd_options = {
-  'response_code':true,
-  'tags': ['app:my_app']
-};
-// eslint-disable-next-line
-const connect_datadog = require('connect-datadog')(dd_options);
-
 app.use(cors());
 app.use(express.json({ limit: '5mb' }));
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
 }
-app.use(connect_datadog);
 
 app.set('trust proxy', 1);
 app.get('/', (_, res) => {
