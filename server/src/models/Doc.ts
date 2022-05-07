ba14bf7404db83400cbcb5eecb1d4488ad56f095
Han Wang <hyw2@cornell.edu>
2022-05-07 16:18:28 -0400

    Add setup for adding new docs

---
+++ server/src/models/Doc.ts
@@ -0,0 +1,14 @@
+import mongoose, { Schema } from 'mongoose';
+
+const DocSchema = new Schema({
+    org: { type: String, required: true },
+    url: { type: String, required: true },
+    method: { type: String, required: true },
+    content: { type: String },
+    lastUpdatedAt: { type: Date, default: Date.now },
+    createdAt: { type: Date, default: Date.now },
+});
+
+const Doc = mongoose.model('Doc', DocSchema, 'docs');
+
+export default Doc;
--- server/src/routes/docs.ts
+++ server/src/routes/docs.ts
@@ -1,12 +1,27 @@
 import express from 'express';
+import Doc from '../models/Doc';
 import { getContentFromWebpage } from '../services/webscraper';
 
 const docsRouter = express.Router();
 
 docsRouter.post('/', async (req, res) => {
   const { url } = req.body;
+  const org = 'mintlify';
   try {
-    const content = await getContentFromWebpage(url);
+    const { content, method } = await getContentFromWebpage(url);
+    await Doc.findOneAndUpdate({
+      org,
+      url
+    },
+    {
+      org,
+      url,
+      method,
+      content,
+    },
+    {
+      upsert: true
+    });
     res.send({content});
   } catch (error) {
     res.status(500).send({error})
