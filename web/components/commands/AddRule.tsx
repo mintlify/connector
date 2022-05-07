418ffac3416257c18ec6f0b6e5b3a5482cf4634d
Han Wang <hyw2@cornell.edu>
2022-05-06 21:08:09 -0400

    Add initial setup for adding rule config

--- web/components/commands/AddRule.tsx
+++ web/components/commands/AddRule.tsx
@@ -1,9 +1,9 @@
-import { Fragment } from 'react'
-import { Combobox, Dialog, Transition } from '@headlessui/react'
+import { Fragment, useState } from 'react'
+import { Combobox, Dialog, RadioGroup, Transition } from '@headlessui/react'
 import { classNames } from '../../helpers/functions'
 import { getRuleTypeIcon } from '../../helpers/Icons';
 import { RuleType } from '../../pages/rules';
-import { ChevronRightIcon } from '@heroicons/react/solid';
+import { ChevronRightIcon, PlusIcon } from '@heroicons/react/solid';
 
 type RuleItem = {
   id: string,
@@ -33,6 +33,12 @@ type AddRuleProps = {
 }
 
 export default function AddRule({ isOpen, setIsOpen }: AddRuleProps) {
+  const [selectedRuleType, setSelectedRuleType] = useState<RuleType | null>(null);
+
+  const onToPrimarySelection = () => {
+    setSelectedRuleType(null);
+  }
+
   return (
     <Transition.Root show={isOpen} as={Fragment} appear>
       <Dialog as="div" className="relative z-10" onClose={setIsOpen}>
@@ -59,7 +65,11 @@ export default function AddRule({ isOpen, setIsOpen }: AddRuleProps) {
             leaveTo="opacity-0 scale-95"
           >
             <Dialog.Panel className="mx-auto max-w-xl transform divide-y divide-gray-100 overflow-hidden rounded-xl bg-white shadow-2xl ring-1 ring-black ring-opacity-5 transition-all">
-              <Combobox onChange={() => {}} value="">
+              { selectedRuleType && (
+                <RuleConfig onCancel={onToPrimarySelection} />
+              ) }
+              {
+                selectedRuleType == null && (<Combobox onChange={() => {}} value="">
                   <Combobox.Options static className="max-h-96 scroll-py-3 overflow-y-auto p-3">
                     {ruleItems.map((item) => (
                       <Combobox.Option
@@ -68,6 +78,7 @@ export default function AddRule({ isOpen, setIsOpen }: AddRuleProps) {
                         className={({ active }) =>
                           classNames('flex items-center cursor-default select-none rounded-xl p-3 hover:cursor-pointer', active ? 'bg-gray-50' : '')
                         }
+                        onClick={() => setSelectedRuleType(item.type)}
                       >
                         {({ active }) => (
                           <>
@@ -94,11 +105,85 @@ export default function AddRule({ isOpen, setIsOpen }: AddRuleProps) {
                       </Combobox.Option>
                     ))}
                   </Combobox.Options>
-              </Combobox>
+              </Combobox>)
+              }
             </Dialog.Panel>
           </Transition.Child>
         </div>
       </Dialog>
     </Transition.Root>
   )
+}
+
+function RuleConfig({ onCancel }: { onCancel: () => void }) {
+  return <form className="px-6 py-5">
+  <div className="space-y-5">
+    <div>
+      <h1 className="text-lg leading-6 font-medium text-gray-900">Require documentation update</h1>
+      <p className="mt-1 text-sm text-gray-500">
+        Enforce updates when relevant code changes
+      </p>
+    </div>
+
+    <div className="space-y-2">
+      <div className="space-y-1">
+        <label htmlFor="add-team-members" className="block text-sm font-medium text-gray-700">
+          Add Team Members
+        </label>
+        <p id="add-team-members-helper" className="sr-only">
+          Search by email address
+        </p>
+        <div className="flex">
+          <div className="flex-grow">
+            <input
+              type="text"
+              name="add-team-members"
+              id="add-team-members"
+              className="block w-full shadow-sm focus:ring-sky-500 focus:border-sky-500 sm:text-sm border-gray-300 rounded-md"
+              placeholder="Email address"
+              aria-describedby="add-team-members-helper"
+            />
+          </div>
+          <span className="ml-3">
+            <button
+              type="button"
+              className="bg-white inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+            >
+              <PlusIcon className="-ml-2 mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+              <span>Add</span>
+            </button>
+          </span>
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <label htmlFor="tags" className="block text-sm font-medium text-gray-700">
+        Tags
+      </label>
+      <input
+        type="text"
+        name="tags"
+        id="tags"
+        className="mt-1 block w-full shadow-sm focus:ring-sky-500 focus:border-sky-500 sm:text-sm border-gray-300 rounded-md"
+      />
+    </div>
+
+    <div className="flex justify-end">
+      <button
+        type="button"
+        className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50"
+        onClick={onCancel}
+      >
+        Back
+      </button>
+      <button
+        type="submit"
+        className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-primary hover:bg-primary"
+      >
+        Create Rule
+      </button>
+    </div>
+  </div>
+</form>
 }
