32176ce96f4ff7bacf47ac7597532eb82c6ab00f
Han Wang <hyw2@cornell.edu>
2022-05-08 18:27:22 -0400

    Add link setup for pages from sidebar

--- web/components/Sidebar.tsx
+++ web/components/Sidebar.tsx
@@ -2,6 +2,7 @@ import { CogIcon } from '@heroicons/react/outline';
 import {
   PlusIcon,
 } from '@heroicons/react/solid'
+import Link from 'next/link';
 import { useState } from 'react'
 import AddDocument from './commands/AddDocument'
 import AddRule from './commands/AddRule'
@@ -62,14 +63,16 @@ export default function Sidebar() {
             </div>
             {/* Meta info */}
             <div className="flex flex-col space-y-6 sm:flex-row sm:space-y-0 sm:space-x-8 xl:flex-col xl:space-x-0 xl:space-y-6">
-              <div className="flex items-center space-x-2">
-                <PlusIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                <span className="text-sm text-gray-500 font-medium">Invite team member</span>
+              <div className="flex items-center space-x-2 cursor-pointer text-gray-500 hover:text-gray-700">
+                <PlusIcon className="h-5 w-5" aria-hidden="true" />
+                <span className="text-sm font-medium">Invite team member</span>
               </div>
-              <div className="flex items-center space-x-2">
-                <CogIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                <span className="text-sm text-gray-500 font-medium">Settings</span>
-              </div>
+              <Link href="/settings">
+                <div className="flex items-center space-x-2 cursor-pointer text-gray-500 hover:text-gray-700">
+                  <CogIcon className="h-5 w-5" aria-hidden="true" />
+                  <span className="text-sm font-medium">Settings</span>
+                </div>
+              </Link>
             </div>
           </div>
         </div>
