dc2c88d14564f2f7a9c310d903a6b00aa63360da
Han Wang <hyw2@cornell.edu>
2022-05-09 17:38:04 -0400

    Add setup for different types of automations

--- web/components/commands/AutomationConfig.tsx
+++ web/components/commands/AutomationConfig.tsx
@@ -1,30 +1,46 @@
-import { Fragment, useState } from 'react'
+import { Fragment, ReactElement, useEffect, useState } from 'react'
 import { Listbox, Transition } from '@headlessui/react'
 import { classNames } from '../../helpers/functions'
 import { getAutomationTypeIcon } from '../../helpers/Icons';
 import { AutomationType } from '../../pages/automations';
-import { BellIcon, CheckIcon, HashtagIcon, LinkIcon, MailIcon, SelectorIcon } from '@heroicons/react/solid';
+import { BellIcon, CheckIcon, HashtagIcon, LinkIcon, MailIcon, SelectorIcon, DocumentTextIcon } from '@heroicons/react/solid';
 import { automationMap } from './AddAutomation';
+import axios from 'axios';
+import { API_ENDPOINT } from '../../helpers/api';
 
-const repos = [
+type Source = {
+  id: string;
+  name: string;
+  icon: ReactElement<any, any>;
+  isDefault?: boolean
+}
+
+const defaultDoc: Source = {
+  id: '0',
+  name: 'Select document',
+  icon: <DocumentTextIcon className="flex-shrink-0 h-4 w-4" />,
+  isDefault: true
+}
+
+const repos: Source[] = [
   {
-    id: 0,
+    id: '0',
     name: 'Select repo',
     icon: <img src="/assets/integrations/github.svg" alt="Slack" className="flex-shrink-0 h-4 w-4" />,
     isDefault: true
   },
   {
-    id: 1, name: 'writer',
+    id: '1',
+    name: 'writer',
     icon: <img src="/assets/integrations/github.svg" alt="Slack" className="flex-shrink-0 h-4 w-4" />
   },
   {
-    id: 2,
+    id: '2',
     name: 'connect',
-    icon: <img src="/assets/integrations/github.svg" alt="Slack"
-    className="flex-shrink-0 h-4 w-4" />
+    icon: <img src="/assets/integrations/github.svg" alt="Slack" className="flex-shrink-0 h-4 w-4" />
   },
   {
-    id: 3,
+    id: '3',
     name: 'backend',
     icon: <img src="/assets/integrations/github.svg" alt="Slack" className="flex-shrink-0 h-4 w-4" />
   },
@@ -75,12 +91,29 @@ const alertChannels = [
 ]
 
 export default function AutomationConfig({ automationType, onCancel }: { automationType: AutomationType, onCancel: () => void }) {
+  const [docs, setDocs] = useState<Source[]>([defaultDoc]);
+  const [selectedDoc, setSelectedDoc] = useState(docs[0])
   const [selectedRepo, setSelectedRepo] = useState(repos[0])
   const [selectedChannel, setSelectedChannel] = useState(alertChannels[0]);
 
   const ruleData = automationMap[automationType];
+  
+  useEffect(() => {
+    axios.get(`${API_ENDPOINT}/routes/docs?org=mintlify`)
+        .then((docsResponse) => {
+          const { docs } = docsResponse.data;
+          const formattedDocs = docs.map((doc: any) => {
+            return {
+              id: doc._id,
+              name: doc.title,
+              icon: <img src={doc.favicon} alt="Slack" className="flex-shrink-0 h-4 w-4 rounded-sm" />
+            }
+          });
+          formattedDocs.unshift(defaultDoc);
 
-  const namePlaceholder = `${selectedChannel.defaultName} when ${selectedRepo.name} changes`;
+          setDocs(formattedDocs);
+        });
+  }, [])
 
   const onBackButton = () => {
     onCancel();
@@ -89,6 +122,17 @@ export default function AutomationConfig({ automationType, onCancel }: { automat
   const onCreateButton = () => {
     // Submit form
   }
+
+  let sourceOptions = docs;
+  let selectedSource = selectedDoc;
+  let setSelectedSource = setSelectedDoc;
+  if (automationType === 'code') {
+    sourceOptions = repos;
+    selectedSource = selectedRepo;
+    setSelectedSource = setSelectedRepo;
+  }
+
+  const namePlaceholder = `${selectedChannel.defaultName} when ${selectedSource.name} changes`;
   
   return <div className="px-6 py-6 z-10">
     <div>
@@ -102,15 +146,15 @@ export default function AutomationConfig({ automationType, onCancel }: { automat
         </div>
       </div>
           <div className="mt-4">
-            <Listbox value={selectedRepo} onChange={setSelectedRepo}>
+            <Listbox value={selectedSource} onChange={setSelectedSource}>
               {({ open }) => (
                 <>
                   <Listbox.Label className="block text-sm font-medium text-gray-700">Trigger</Listbox.Label>
                   <div className="mt-1 relative">
                     <Listbox.Button className="relative w-full bg-white hover:bg-gray-50 border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-pointer focus:outline-none sm:text-sm">
                       <span className="flex items-center">
-                        {selectedRepo.icon}
-                        <span className="ml-3 block truncate">{selectedRepo.name}</span>
+                        {selectedSource.icon}
+                        <span className="ml-3 block truncate">{selectedSource.name}</span>
                       </span>
                       <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
                         <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
@@ -125,25 +169,25 @@ export default function AutomationConfig({ automationType, onCancel }: { automat
                       leaveTo="opacity-0"
                     >
                       <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
-                        {repos.map((repo) => (
+                        {sourceOptions.map((sourceOption) => (
                           <Listbox.Option
-                            key={repo.id}
+                            key={sourceOption.id}
                             className={({ active }) =>
                               classNames(
                                 active ? 'text-white bg-primary' : 'text-gray-900',
                                 'hover:cursor-pointer select-none relative py-2 pl-3 pr-9'
                               )
                             }
-                            value={repo}
+                            value={sourceOption}
                           >
                             {({ selected, active }) => (
                               <>
                                 <div className="flex items-center">
-                                  {repo.icon}
+                                  {sourceOption.icon}
                                   <span
                                     className={classNames(selected ? 'font-medium' : 'font-normal', 'ml-3 block truncate')}
                                   >
-                                    {repo.name}
+                                    {sourceOption.name}
                                   </span>
                                 </div>
 
@@ -168,27 +212,31 @@ export default function AutomationConfig({ automationType, onCancel }: { automat
               )}
             </Listbox>
 
-            {!selectedRepo?.isDefault && (
+            {!selectedSource?.isDefault && (
               <div>
-              <fieldset className="space-y-3">
-              <legend className="sr-only">Notifications</legend>
-              <div className="relative flex items-start">
-                <div className="flex items-center h-5">
-                  <input
-                    id="comments"
-                    aria-describedby="comments-description"
-                    name="comments"
-                    type="checkbox"
-                    className="focus:ring-0 h-4 w-4 text-primary border-gray-300 rounded"
-                  />
-                </div>
-                <div className="ml-3 text-sm">
-                  <span id="comments-description" className="text-gray-500">
-                    Only get triggered by <span className="underline decoration-dashed decoration-gray-300">connected code</span>
-                  </span>
-                </div>
-              </div>
-            </fieldset>
+                {
+                  automationType === 'code' && (
+                    <fieldset className="space-y-3">
+                    <legend className="sr-only">Notifications</legend>
+                    <div className="relative flex items-start">
+                      <div className="flex items-center h-5">
+                        <input
+                          id="comments"
+                          aria-describedby="comments-description"
+                          name="comments"
+                          type="checkbox"
+                          className="focus:ring-0 h-4 w-4 text-primary border-gray-300 rounded"
+                        />
+                      </div>
+                    <div className="ml-3 text-sm">
+                      <span id="comments-description" className="text-gray-500">
+                        Only get triggered by <span className="underline decoration-dashed decoration-gray-300">connected code</span>
+                      </span>
+                    </div>
+                    </div>
+                    </fieldset>
+                  )
+                }
             <div className="mt-4">
             <Listbox value={selectedChannel} onChange={setSelectedChannel}>
               {({ open }) => (
@@ -264,7 +312,7 @@ export default function AutomationConfig({ automationType, onCancel }: { automat
               </label>
               <div className="mt-1 relative rounded-md shadow-sm">
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <selectedChannel.destination.icon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                  <selectedChannel.destination.icon className="h-4 w-4 text-gray-400" aria-hidden="true" />
                 </div>
                 <input
                   type="text"
