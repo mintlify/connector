c718ce2b49462c0191fe820b0a38533a2b863901
Peter Nguyen <peternguyenforwork@gmail.com>
2022-05-16 12:46:37 -0700

    direct the invite btn on the sidebar to organization under the setting page

--- web/components/Sidebar.tsx
+++ web/components/Sidebar.tsx
@@ -77,10 +77,12 @@ export default function Sidebar({ user, isAddingDoc, setIsAddingDoc }: SidebarPr
             </div>
             {/* Meta info */}
             <div className="flex flex-col space-y-6 sm:flex-row sm:space-y-0 sm:space-x-8 xl:flex-col xl:space-x-0 xl:space-y-6">
-              <div className="flex items-center space-x-2 cursor-pointer text-gray-500 hover:text-gray-700">
-                <PlusIcon className="h-5 w-5" aria-hidden="true" />
-                <span className="text-sm font-medium">Invite team member</span>
-              </div>
+              <Link href="/settings#setting-organization">
+                <div className="flex items-center space-x-2 cursor-pointer text-gray-500 hover:text-gray-700">
+                  <PlusIcon className="h-5 w-5" aria-hidden="true" />
+                  <span className="text-sm font-medium">Invite team member</span>
+                </div>
+              </Link>
               <Link href="/settings">
                 <div className="flex items-center space-x-2 cursor-pointer text-gray-500 hover:text-gray-700">
                   <CogIcon className="h-5 w-5" aria-hidden="true" />
