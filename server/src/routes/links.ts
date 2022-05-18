4063d4bd89326c78ebcf4ced3daaf38cfe5a83db
Han Wang <hyw2@cornell.edu>
2022-05-17 23:18:00 -0400

    Remove unneeded post request for link

--- server/src/routes/links.ts
+++ server/src/routes/links.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import Code, { CodeType } from '../models/Code';
-import { createDocFromUrl } from './docs';
 import { userMiddleware } from './user';
 
 const linksRouter = express.Router();
@@ -12,20 +11,6 @@ linksRouter.put('/', userMiddleware, async (req, res) => {
         return Code.create(code);
     });
     await Promise.all(codePromises);
-    return res.send({success: true});
-});
-
-linksRouter.post('/', userMiddleware, async (req, res) => {
-    const { url, org, codes } = req.body;
-
-    const { doc } = await createDocFromUrl(url, org, res.locals.user._id);
-    if (doc) {
-        const codePromises: Promise<CodeType>[] = codes.map((code: CodeType) => {
-            code.doc = doc._id;
-            return Code.create(code);
-        });
-        await Promise.all(codePromises);
-    }
     return res.send({success: true});
 });
 
