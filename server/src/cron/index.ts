5723c505d52ce345e412622a8a8c2deda3341b53
Han Wang <hyw2@cornell.edu>
2022-05-20 00:02:08 -0400

    Add scraping based on words

--- server/src/cron/index.ts
+++ server/src/cron/index.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 cron.schedule('* * * * *', async () => {
   console.log('running scan');
   const { diffAlerts }: any = await axios.post('http://localhost:5000/routes/scan', {
-    org: 'mintlify'
+    org: '628178196ecd0d1f626b7cf1'
   });
 
   console.log(`scan completed. Found ${diffAlerts.length} alerts`);
