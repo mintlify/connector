5adb313aa9b9b9cc9a07d932c5f687c8cf5b5787
Peter Nguyen <peternguyenforwork@gmail.com>
2022-05-21 00:03:15 -0700

    WIP | having a weird bug in parsing google docs url

--- server/src/services/googleDocs.ts
+++ server/src/services/googleDocs.ts
@@ -12,13 +12,62 @@ const jwt = {
 };
 
 const auth = new google.auth.JWT(jwt.client_email, undefined, jwt.private_key, scopes, jwt.auth_email);
-export const docs = google.docs({ version: "v1", auth });
+
+export type ParagraphElement = {
+  startIndex: number;
+  endIndex: number;
+  textRun: {
+    content: string;
+    textStyle: {};
+  };
+};
+
+export type Paragraph = {
+  elements: ParagraphElement[];
+  paragraphStyle?: any;
+};
 
 export const isGoogleDocsUrl = (url: URL): boolean => url.host === "docs.google.com" || url.host === "www.docs.google.com";
 
 export const getGoogleDocsTitle = async (url: URL): Promise<string> => {
-  const response = await docs.documents.get({
-    documentId: url.pathname.split("/")[2],
-  });
-  return response.data.title ? response.data.title : "Error: Title not found";
+  const docs = google.docs({ version: "v1", auth });
+  console.log("url.pathname", url.pathname);
+  console.log("url.pathname.split('/')[3] = ", url.pathname.split("/")[3]);
+  try {
+    const documentId = url.pathname.split("/")[3].toString();
+    const response = await docs.documents.get({ documentId });
+    return response.data.title ? response.data.title : "";
+  } catch (err) {
+    console.log("error catched");
+    console.log(err);
+    return "Title not found";
+  }
+};
+
+export const getGoogleDocsContent = async (): Promise<string> => {
+  try {
+    const documentId: string = "1vYV9TUw8VFpuGatfgmOvcRSpHxuDkuN0zBS4p2GIQoU";
+    console.log(`documentId = |${documentId}|`);
+    console.log("auth = ", auth);
+    const docs = google.docs({ version: "v1", auth });
+    const res = await docs.documents.get({ documentId });
+    console.log("Got res!");
+    if (!res.data.body) return "";
+    const content: any = res.data.body?.content;
+    if (!content) return "";
+    let accumulateContent = "";
+
+    content
+      .filter((block: any) => block.hasOwnProperty("paragraph"))
+      .map(({ paragraph }: { paragraph: Paragraph }) => {
+        paragraph.elements.map(
+          ({ textRun: { content } }: { textRun: { content: string } }) => (accumulateContent = `${accumulateContent}${content}`)
+        );
+      });
+
+    return accumulateContent;
+  } catch (err) {
+    console.log(err);
+    return "Dummy content";
+  }
 };
