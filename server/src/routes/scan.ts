6edfad95cfb755f9cc13c0a1bb8b919f0a46f34c
Han Wang <44352119+hanywang2@users.noreply.github.com>
2022-05-21 00:04:58 -0400

    Worker (#67)
    * Add setup for individual scan
    
    * Add global scanning
    
    * Add worker dynos

--- server/src/routes/scan.ts
+++ server/src/routes/scan.ts
@@ -5,6 +5,9 @@ import Event, { EventType } from '../models/Event';
 import { getDataFromWebpage } from '../services/webscraper';
 import { triggerAutomationsForEvents } from '../automations';
 import { updateDocContentForSearch } from '../services/algolia';
+import { workQueue } from '../workers';
+import mongoose from 'mongoose';
+import Org from '../models/Org';
 
 const scanRouter = express.Router();
 
@@ -27,63 +30,97 @@ const getDiffAndContent = async (url: string, previousContent: string, orgId: st
   }
 }
 
-scanRouter.post('/', async (req, res) => {
-  const { org } = req.body;
+export const scanDocsInOrg = async (orgId: string) => {
+  const docsFromOrg = await Doc.find({ org: orgId });
+  const getDifferencePromises = docsFromOrg.map((doc) => {
+    return getDiffAndContent(doc.url, doc.content ?? '', orgId);
+  });
+
+  const diffsAndContent = await Promise.all(getDifferencePromises);
+
+  const diffAlerts: DiffAlert[] = [];
+  diffsAndContent.forEach(({ diff, newContent }, i) => {
+    const hasChanges = diff.some((diff) => diff.added || diff.removed);
+    if (hasChanges) {
+      diffAlerts.push({
+        doc: docsFromOrg[i],
+        newContent,
+        diff
+      });
+    }
+  });
+
+  const docUpdateQueries = diffAlerts.map(({ doc, newContent }) => {
+    return {
+      updateOne: {
+        filter: { _id: doc._id },
+        update: { content: newContent, lastUpdatedAt: new Date() }
+      }
+    }
+  })
+
+  const bulkWriteDocsPromise = Doc.bulkWrite(docUpdateQueries, {
+    ordered: false
+  });
+
+  const newEvents: EventType[] = diffAlerts.map((alert) => {
+    return {
+      org: new mongoose.Types.ObjectId(orgId),
+      doc: alert.doc._id,
+      type: 'change',
+      change: alert.diff,
+    }
+  });
+  const insertManyEventsPromise = Event.insertMany(newEvents);
+  const updateSearchDocRecordsPromises: Promise<void>[] = diffAlerts.map(({ doc, newContent}) => {
+    return updateDocContentForSearch(doc, newContent);
+  });
+  const [_, insertManyEventsRes] = await Promise.all([bulkWriteDocsPromise, insertManyEventsPromise, updateSearchDocRecordsPromises]);
+  await triggerAutomationsForEvents(orgId, insertManyEventsRes);
+
+  return diffAlerts;
+}
+
+scanRouter.post('/org/:orgId', async (req, res) => {
+  const { orgId } = req.params;
 
   try {
-    const docsFromOrg = await Doc.find({ org });
-    const getDifferencePromises = docsFromOrg.map((doc) => {
-      return getDiffAndContent(doc.url, doc.content ?? '', org);
-    });
-
-    const diffsAndContent = await Promise.all(getDifferencePromises);
-
-    const diffAlerts: DiffAlert[] = [];
-    diffsAndContent.forEach(({ diff, newContent }, i) => {
-      const hasChanges = diff.some((diff) => diff.added || diff.removed);
-      if (hasChanges) {
-        diffAlerts.push({
-          doc: docsFromOrg[i],
-          newContent,
-          diff
-        });
-      }
-    });
-
-    const docUpdateQueries = diffAlerts.map(({ doc, newContent }) => {
-      return {
-        updateOne: {
-          filter: { _id: doc._id },
-          update: { content: newContent, lastUpdatedAt: new Date() }
-        }
-      }
-    })
-
-    const bulkWriteDocsPromise = Doc.bulkWrite(docUpdateQueries, {
-      ordered: false
-    });
-
-    const newEvents: EventType[] = diffAlerts.map((alert) => {
-      return {
-        org,
-        doc: alert.doc._id,
-        type: 'change',
-        change: alert.diff,
-      }
-    });
-    const insertManyEventsPromise = Event.insertMany(newEvents);
-    const updateSearchDocRecordsPromises: Promise<void>[] = diffAlerts.map(({ doc, newContent}) => {
-      return updateDocContentForSearch(doc, newContent);
-    });
-    const [_, insertManyEventsRes] = await Promise.all([bulkWriteDocsPromise, insertManyEventsPromise, updateSearchDocRecordsPromises]);
-    await triggerAutomationsForEvents(org, insertManyEventsRes);
-   
-    res.send({diffAlerts});
+    const job = await workQueue.add({orgId});
+    res.status(200).json({ id: job.id })
   }
-
   catch (error) {
     res.status(500).send({error})
   }
+});
+
+scanRouter.get('/status/:jobId', async (req, res) => {
+  const { jobId } = req.params;
+
+  const job = await workQueue.getJob(jobId);
+  
+  if (job === null) {
+    res.status(404).end();
+  } else {
+    const state = await job.getState();
+    let data;
+    if (state === 'completed') {
+      data = await job.finished();
+    }
+    const reason = job.failedReason;
+    res.json({ jobId, state, reason, data });
+  }
+})
+
+scanRouter.put('/global', async (_, res) => {
+  const orgs = await Org.find({});
+  const orgIds = orgs.map((org) => org._id);
+  const startWorkerPromises = orgIds.map((orgId) => {
+    return workQueue.add({orgId});
+  })
+
+  const jobs = await Promise.all(startWorkerPromises);
+  const jobsIds = jobs.map((job) => job.id);
+  res.send({jobsIds});
 });
 
 export default scanRouter;
