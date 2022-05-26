e32e72d007d8bf4aa0a0eba3f0b24d8f4879c45f
Han Wang <hyw2@cornell.edu>
2022-05-26 19:37:10 -0400

    Remove all alerts from V01

--- server/src/parsing/languages/kotlin/index.ts
+++
@@ -1,8 +0,0 @@
-import { PLConnect, TreeNode } from '../../types';
-import { extractBaseComments } from '../helpers';
-
-export default class Kotlin implements PLConnect {  
-  extractComment(tree: TreeNode): string | null {
-    return extractBaseComments(tree);
-  }
-}
--- server/src/parsing/languages/typescript/index.ts
+++
@@ -1,8 +0,0 @@
-import { PLConnect, TreeNode } from '../../types';
-import { extractBaseComments } from '../helpers';
-
-export default class TypeScript implements PLConnect {  
-  extractComment(tree: TreeNode) {
-    return extractBaseComments(tree);
-  }
-}
