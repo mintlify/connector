e858751f25e58dee7a0a78e1bb6ff993e26f3911
Han Wang <44352119+hanywang2@users.noreply.github.com>
2022-06-01 12:22:59 -0400

    Webflow (#80)
    * Add setup for creating org
    
    * Add redirect url
    
    * Add check for available org

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,15 +1,15 @@
 import mongoose, { Schema } from 'mongoose';
 
 const UserSchema = new Schema({
-    userId: { type: String },
+    userId: { type: String, required: true },
     authMethod: { type: String, default: 'stytch' },
-    firstName: { type: String},
-    lastName: { type: String},
+    firstName: { type: String, required: true, },
+    lastName: { type: String, required: true },
     email: { type: String, required: true },
     profilePicture: { type: String },
     createdAt: { type: Date, default: Date.now },
     role: { type: String },
-    pending: {type: Boolean, default: false, required: true},
+    pending: { type: Boolean, default: false, required: true },
     isVSCodeInstalled: { type: Boolean, default: false }
 });
 
