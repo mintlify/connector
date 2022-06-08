25a474a0e2d1dc05ad61da32f18bcd3cdcde707a
Peter Nguyen <peternguyenforwork@gmail.com>
2022-06-08 13:02:01 -0700

    add a boilerplate for the google integration

---
+++ server/src/routes/integrations/google.ts
@@ -0,0 +1,10 @@
+// import axios from 'axios';
+import { Router } from 'express';
+
+const googleRouter = Router();
+
+googleRouter.get('/install', async (req, res) => {});
+
+googleRouter.get('/authorization', async (req, res) => {});
+
+export default googleRouter;
