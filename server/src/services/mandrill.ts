78cb1fd7faa08ce9ed4d13288913d7332fe9c4df
Hahnbee Lee <hl985@cornell.edu>
2022-06-09 13:26:57 -0400

    refactor backend automations

--- server/src/services/mandrill.ts
+++ server/src/services/mandrill.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { OrgType } from "../models/Org";
+import User from "../models/User";
 
 export type GlobalMergeVars = {
   name: string;
@@ -17,6 +19,30 @@ export const sendEmail = async (
     template_content: [],
     message: {
       to: [{ email: toEmail }],
+      from_email: fromEmail,
+      from_name: fromEmail,
+      merge_language: "handlebars",
+      global_merge_vars,
+    },
+  });
+}
+
+export const sendEmailToAllMembersOfOrg = async (
+  org: OrgType,
+  fromEmail: string = "hi@mintlify.com",
+  template_name: string = "doc-change-alert",
+  global_merge_vars: GlobalMergeVars[] = []
+) => {
+  const users = await User.find({ userId: org.users });
+  const formattedUsers = users.map((user) => {
+   return { email: user.email };
+  });
+  await axios.post("https://mandrillapp.com/api/1.0/messages/send-template", {
+    key: process.env.MAILCHIMP_TRANSACTIONAL_KEY,
+    template_name,
+    template_content: [],
+    message: {
+      to: formattedUsers,
       from_email: fromEmail,
       from_name: fromEmail,
       merge_language: "handlebars",
