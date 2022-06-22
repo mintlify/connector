d96dcf96d8774237d033b5d08ed42180f9f704a7
Han Wang <hyw2@cornell.edu>
2022-06-22 15:42:48 -0400

    Add initial routes for getting and creating tasks

---
+++ server/src/routes/tasks.ts
@@ -0,0 +1,55 @@
+import { Router } from 'express';
+import { Types } from 'mongoose';
+import Doc from '../models/Doc';
+import Task from '../models/Task';
+import { userMiddleware } from './user';
+
+const tasksRouter = Router();
+
+tasksRouter.get('/', userMiddleware, async (_, res) => {
+  const { org } = res.locals.user;
+
+  const tasks = await Task.aggregate([
+    {
+      $match: {
+        org: org._id,
+      }
+    },
+    {
+      $lookup: {
+        from: 'docs',
+        localField: 'doc',
+        foreignField: '_id',
+        as: 'doc'
+      }
+    },
+    {
+      $set: {
+        doc: { $first: "$doc" },
+      },
+    }
+  ]);
+  res.send({ tasks });
+});
+
+tasksRouter.post('/update/:docId', userMiddleware, async (req, res) => {
+  const { docId } = req.params;
+  const { org } = res.locals.user;
+
+  const doc = await Doc.findById(docId);
+
+  if (doc == null) {
+    return res.status(400).send({error: 'No doc with DocId found'});
+  }
+
+  await Task.create({
+    org: org._id,
+    doc: new Types.ObjectId(docId),
+    status: 'todo',
+    type: 'update',
+    url: doc.url,
+  })
+  return res.end();
+});
+
+export default tasksRouter;
--- web/components/ActivityBar.tsx
+++ web/components/ActivityBar.tsx
@@ -1,10 +1,12 @@
-import { ArrowRightIcon, PencilIcon, XCircleIcon } from '@heroicons/react/solid';
+import { CheckIcon } from '@heroicons/react/outline';
+import { PencilIcon, XCircleIcon } from '@heroicons/react/solid';
 import axios from 'axios';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { useProfile } from '../context/ProfileContext';
 import { API_ENDPOINT } from '../helpers/api';
 import { ConnectionIcon, DocTitleIcon } from '../helpers/Icons';
+import { request } from '../helpers/request';
 import { getSubdomain } from '../helpers/user';
 import { Doc } from '../pages';
 import timeAgo from '../services/timeago';
@@ -50,6 +52,13 @@ function DocProfile({ doc }: DocProfileProps) {
       }
     });
   };
+
+  const onCreateUpdateRequest = () => {
+    request('POST', `/routes/tasks/update/${doc._id}`)
+      .then(() => {
+        console.log('Created');
+      })
+  }
 
   const vscodeUrl = isVSCodeInstalled ? `vscode://mintlify.connector/prefill-doc?docId=${doc._id}` : 'vscode:extension/mintlify.connector';
 
@@ -103,15 +112,14 @@ function DocProfile({ doc }: DocProfileProps) {
             </Link>
           </div>
           <div>
-            <Link href={vscodeUrl}>
               <button
-                type="button"
-                className="inline-flex items-center justify-center py-1 border border-gray-300 shadow-sm text-xs font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 w-full"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-3 mr-1.5" viewBox="0 0 512 512"><path d="M490.3 40.4C512.2 62.27 512.2 97.73 490.3 119.6L460.3 149.7L362.3 51.72L392.4 21.66C414.3-.2135 449.7-.2135 471.6 21.66L490.3 40.4zM172.4 241.7L339.7 74.34L437.7 172.3L270.3 339.6C264.2 345.8 256.7 350.4 248.4 353.2L159.6 382.8C150.1 385.6 141.5 383.4 135 376.1C128.6 370.5 126.4 361 129.2 352.4L158.8 263.6C161.6 255.3 166.2 247.8 172.4 241.7V241.7zM192 63.1C209.7 63.1 224 78.33 224 95.1C224 113.7 209.7 127.1 192 127.1H96C78.33 127.1 64 142.3 64 159.1V416C64 433.7 78.33 448 96 448H352C369.7 448 384 433.7 384 416V319.1C384 302.3 398.3 287.1 416 287.1C433.7 287.1 448 302.3 448 319.1V416C448 469 405 512 352 512H96C42.98 512 0 469 0 416V159.1C0 106.1 42.98 63.1 96 63.1H192z"/></svg>
-                Create Update Request
-              </button>
-            </Link>
+              type="button"
+              className="inline-flex items-center justify-center py-1 border border-gray-300 shadow-sm text-xs font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 w-full"
+              onClick={onCreateUpdateRequest}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-3 mr-1.5" viewBox="0 0 512 512"><path d="M490.3 40.4C512.2 62.27 512.2 97.73 490.3 119.6L460.3 149.7L362.3 51.72L392.4 21.66C414.3-.2135 449.7-.2135 471.6 21.66L490.3 40.4zM172.4 241.7L339.7 74.34L437.7 172.3L270.3 339.6C264.2 345.8 256.7 350.4 248.4 353.2L159.6 382.8C150.1 385.6 141.5 383.4 135 376.1C128.6 370.5 126.4 361 129.2 352.4L158.8 263.6C161.6 255.3 166.2 247.8 172.4 241.7V241.7zM192 63.1C209.7 63.1 224 78.33 224 95.1C224 113.7 209.7 127.1 192 127.1H96C78.33 127.1 64 142.3 64 159.1V416C64 433.7 78.33 448 96 448H352C369.7 448 384 433.7 384 416V319.1C384 302.3 398.3 287.1 416 287.1C433.7 287.1 448 302.3 448 319.1V416C448 469 405 512 352 512H96C42.98 512 0 469 0 416V159.1C0 106.1 42.98 63.1 96 63.1H192z"/></svg>
+              Create Update Request
+            </button>
           </div>
           <div>
           </div>
@@ -121,11 +129,33 @@ function DocProfile({ doc }: DocProfileProps) {
   </div>
 }
 
+export type Task = {
+  _id: string
+  org: string
+  doc: Doc
+  code: string
+  status: 'todo' | 'done'
+  type: 'new' | 'update' | 'review'
+  url?: string
+  createdAt: string
+}
+
 type ActivityBarProps = {
   selectedDoc?: Doc;
 }
 
 export default function ActivityBar({ selectedDoc }: ActivityBarProps) {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  
+  useEffect(() => {
+    request('GET', 'routes/tasks')
+      .then(({ data }) => {
+        const { tasks } = data;
+        setTasks(tasks);
+      })
+  }, []);
+
+  console.log({tasks});
   return (
     <div className="relative pl-6 lg:w-80">
       {selectedDoc && <DocProfile doc={selectedDoc} />}
@@ -137,34 +167,33 @@ export default function ActivityBar({ selectedDoc }: ActivityBarProps) {
            <div className="text-sm text-gray-600 space-y-4 py-2">
             <div className="flow-root">
               <ul role="list" className="-my-4 divide-y divide-gray-200">
-              <li className="flex items-center py-4 space-x-3">
-                <div className="flex items-center justify-center bg-red-500 border-2 border-red-300 p-1.5 rounded-full">
-                  <PencilIcon className="h-4 w-4 text-white" />
-                </div>
-                <div className="min-w-0 flex-1">
-                  <p className="text-sm font-medium text-gray-900">
-                    <a>Update Onboarding</a>
-                  </p>
-                  <p className="text-sm text-gray-500">
-                    Created 30 min ago
-                  </p>
-                </div>
-                <div className="flex-shrink-0">
-                  <button
-                    type="button"
-                    className="items-center justify-center py-1 px-2 border border-gray-300 shadow-sm text-xs font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 w-full"
-                  >
-                    <span>Resolve</span>
-                  </button>
-                </div>
-              </li>
+              {
+                tasks.map((task) => 
+                  <li key={task._id} className="flex items-center py-4 space-x-3">
+                  <div className="flex items-center justify-center bg-red-500 border-2 border-red-300 p-1.5 rounded-full">
+                    <PencilIcon className="h-4 w-4 text-white" />
+                  </div>
+                  <div className="min-w-0 flex-1">
+                    <p className="text-sm font-medium text-gray-900">
+                      <a>Update {task.doc.title}</a>
+                    </p>
+                    <p className="text-sm text-gray-500">
+                      Created {timeAgo.format(Date.parse(task.createdAt))}
+                    </p>
+                  </div>
+                  <div className="flex-shrink-0">
+                    <button
+                      type="button"
+                      className="items-center justify-center py-1 px-2 border border-gray-300 shadow-sm text-xs font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 w-full"
+                    >
+                      <span><CheckIcon className="h-4 text-green-700" /></span>
+                    </button>
+                  </div>
+                </li>)
+              }
               </ul>
             </div>
             </div>
-        </div>
-        <div className="mt-5 text-sm font-medium text-gray-500 flex items-center">
-          Connect with task management
-          <ArrowRightIcon className="ml-1 h-3" />
         </div>
       </div>
   )
