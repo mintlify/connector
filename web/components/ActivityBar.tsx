ca99ccb685f1ae5c4b168508d8c63bddc477fdab
Han Wang <hyw2@cornell.edu>
2022-06-22 20:02:06 -0400

    Mark tasks from selected docs

--- web/components/ActivityBar.tsx
+++ web/components/ActivityBar.tsx
@@ -173,6 +173,8 @@ export default function ActivityBar({ selectedDoc, refresh, refreshKey }: Activi
         refresh();
       })
   }
+
+  const relevantTasks = selectedDoc ? tasks.filter((task) => task.doc._id === selectedDoc._id) : tasks;
   
   return (
     <div className="relative pl-6 lg:w-80">
@@ -186,15 +188,17 @@ export default function ActivityBar({ selectedDoc, refresh, refreshKey }: Activi
             <div className="flow-root">
               <ul role="list" className="-my-4 divide-y divide-gray-200">
               {
-                tasks.map((task) => 
+                relevantTasks.map((task) => 
                   <li key={task._id} className="flex items-center py-4 space-x-3">
                   <div className="flex items-center justify-center bg-red-500 border-2 border-red-300 p-1.5 rounded-full">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 text-white" viewBox="0 0 512 512" fill="currentColor"><path d="M490.3 40.4C512.2 62.27 512.2 97.73 490.3 119.6L460.3 149.7L362.3 51.72L392.4 21.66C414.3-.2135 449.7-.2135 471.6 21.66L490.3 40.4zM172.4 241.7L339.7 74.34L437.7 172.3L270.3 339.6C264.2 345.8 256.7 350.4 248.4 353.2L159.6 382.8C150.1 385.6 141.5 383.4 135 376.1C128.6 370.5 126.4 361 129.2 352.4L158.8 263.6C161.6 255.3 166.2 247.8 172.4 241.7V241.7zM192 63.1C209.7 63.1 224 78.33 224 95.1C224 113.7 209.7 127.1 192 127.1H96C78.33 127.1 64 142.3 64 159.1V416C64 433.7 78.33 448 96 448H352C369.7 448 384 433.7 384 416V319.1C384 302.3 398.3 287.1 416 287.1C433.7 287.1 448 302.3 448 319.1V416C448 469 405 512 352 512H96C42.98 512 0 469 0 416V159.1C0 106.1 42.98 63.1 96 63.1H192z"/></svg>
                   </div>
                   <div className="min-w-0 flex-1">
-                    <p className="text-sm font-medium text-gray-900">
-                      <a>Update {task.doc.title}</a>
-                    </p>
+                    <Link href={task.url || ''}>
+                      <a target="_blank" className="text-sm font-medium text-gray-700 hover:underline cursor-pointer">
+                        <span>Update {task.doc.title}</span>
+                      </a>
+                    </Link>
                     <p className="text-sm text-gray-500">
                       Created {timeAgo.format(Date.parse(task.createdAt))}
                     </p>
@@ -211,7 +215,7 @@ export default function ActivityBar({ selectedDoc, refresh, refreshKey }: Activi
                 </li>)
               }
               {
-                tasks.length === 0 && <div className="mt-4">
+                relevantTasks.length === 0 && <div className="mt-4">
                   No update requests 🎉
                 </div>
               }
