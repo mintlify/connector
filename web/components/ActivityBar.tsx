f25bb4c65e5356844e7a36b7a50874df49fb2b27
Han Wang <hyw2@cornell.edu>
2022-06-23 21:16:30 -0400

    Add GitHub review icon

--- web/components/ActivityBar.tsx
+++ web/components/ActivityBar.tsx
@@ -1,4 +1,3 @@
-import { CheckIcon } from '@heroicons/react/outline';
 import { XCircleIcon } from '@heroicons/react/solid';
 import axios from 'axios';
 import Link from 'next/link';
@@ -13,6 +12,7 @@ import { request } from '../helpers/request';
 import { getSubdomain } from '../helpers/user';
 import { Doc } from '../pages';
 import timeAgo from '../services/timeago';
+import TaskItem from './TaskItem';
 import Tooltip from './Tooltip';
 
 type DocProfileProps = {
@@ -192,30 +192,8 @@ export default function ActivityBar({ selectedDoc, refresh, refreshKey }: Activi
               <ul role="list" className="-my-4 divide-y divide-gray-200">
               {
                 relevantTasks.map((task) => 
-                  <li key={task._id} className="flex items-center py-4 space-x-3">
-                  <div className="flex items-center justify-center bg-red-500 border-2 border-red-300 p-1.5 rounded-full">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 text-white" viewBox="0 0 512 512" fill="currentColor"><path d="M490.3 40.4C512.2 62.27 512.2 97.73 490.3 119.6L460.3 149.7L362.3 51.72L392.4 21.66C414.3-.2135 449.7-.2135 471.6 21.66L490.3 40.4zM172.4 241.7L339.7 74.34L437.7 172.3L270.3 339.6C264.2 345.8 256.7 350.4 248.4 353.2L159.6 382.8C150.1 385.6 141.5 383.4 135 376.1C128.6 370.5 126.4 361 129.2 352.4L158.8 263.6C161.6 255.3 166.2 247.8 172.4 241.7V241.7zM192 63.1C209.7 63.1 224 78.33 224 95.1C224 113.7 209.7 127.1 192 127.1H96C78.33 127.1 64 142.3 64 159.1V416C64 433.7 78.33 448 96 448H352C369.7 448 384 433.7 384 416V319.1C384 302.3 398.3 287.1 416 287.1C433.7 287.1 448 302.3 448 319.1V416C448 469 405 512 352 512H96C42.98 512 0 469 0 416V159.1C0 106.1 42.98 63.1 96 63.1H192z"/></svg>
-                  </div>
-                  <div className="min-w-0 flex-1">
-                    <Link href={task.url || ''}>
-                      <a target="_blank" className="text-sm font-medium text-gray-700 hover:underline cursor-pointer">
-                        <span>Update {task.doc.title}</span>
-                      </a>
-                    </Link>
-                    <p className="text-sm text-gray-500">
-                      Created {timeAgo.format(Date.parse(task.createdAt))}
-                    </p>
-                  </div>
-                  <div className="flex-shrink-0">
-                    <button
-                      type="button"
-                      className="items-center justify-center py-1 px-2 border border-gray-300 shadow-sm text-xs font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 w-full"
-                      onClick={() => onCompleteTask(task)}
-                    >
-                      <span><CheckIcon className="h-4 text-green-700" /></span>
-                    </button>
-                  </div>
-                </li>)
+                  <TaskItem key={task._id} task={task} onCompleteTask={onCompleteTask} />
+                )
               }
               {
                 relevantTasks.length === 0 && <div className="mt-4">
---
+++ web/components/TaskItem.tsx
@@ -0,0 +1,50 @@
+import { CheckIcon } from "@heroicons/react/solid";
+import Link from "next/link";
+import timeAgo from "../services/timeago";
+import { Task } from "./ActivityBar";
+
+export default function TaskItem({ task, onCompleteTask }: { task: Task, onCompleteTask: (task: Task) => void }) {
+  let taskIcon;
+  let actionLabel;
+  switch (task.type) {
+    case 'update':
+      taskIcon = <div className="flex items-center justify-center bg-red-500 p-1.5 rounded-full">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 text-white" viewBox="0 0 512 512" fill="currentColor">
+          <path d="M362.7 19.32C387.7-5.678 428.3-5.678 453.3 19.32L492.7 58.75C517.7 83.74 517.7 124.3 492.7 149.3L444.3 197.7L314.3 67.72L362.7 19.32zM421.7 220.3L188.5 453.4C178.1 463.8 165.2 471.5 151.1 475.6L30.77 511C22.35 513.5 13.24 511.2 7.03 504.1C.8198 498.8-1.502 489.7 .976 481.2L36.37 360.9C40.53 346.8 48.16 333.9 58.57 323.5L291.7 90.34L421.7 220.3z"/>
+        </svg>
+      </div>
+      actionLabel = 'Update';
+      break;
+    case 'review':
+      taskIcon = <div className="flex items-center justify-center bg-white p-1 rounded-full">
+        <img src="assets/integrations/github.svg" className="h-6" alt="GitHub" />
+      </div>
+      actionLabel = 'Review';
+      break;
+    default:
+      taskIcon = null;
+  }
+
+  return <li key={task._id} className="flex items-center py-4 space-x-3">
+  {taskIcon}
+  <div className="min-w-0 flex-1">
+    <Link href={task.url || ''}>
+      <a target="_blank" className="text-sm font-medium text-gray-700 hover:underline cursor-pointer">
+        <span>{actionLabel} {task.doc.title}</span>
+      </a>
+    </Link>
+    <p className="text-sm text-gray-500">
+      Created {timeAgo.format(Date.parse(task.createdAt))}
+    </p>
+  </div>
+  <div className="flex-shrink-0">
+    <button
+      type="button"
+      className="items-center justify-center py-1 px-2 border border-gray-300 shadow-sm text-xs font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 w-full"
+      onClick={() => onCompleteTask(task)}
+    >
+      <span><CheckIcon className="h-4 text-green-700" /></span>
+    </button>
+  </div>
+</li>
+}
