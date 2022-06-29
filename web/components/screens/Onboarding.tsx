bf7ae90845cc310ee60ec7378a01264caa753e22
Han Wang <hyw2@cornell.edu>
2022-06-29 16:42:53 -0400

    Add workflow demo

--- web/components/screens/Onboarding.tsx
+++ web/components/screens/Onboarding.tsx
@@ -266,7 +266,7 @@ function InstallGitHubStep({ user, org, onBack, onNext, step }: { user: User, or
     <ProgressBar step={step} /> 
     <div className="mt-6">
       <video className="w-full rounded-sm" autoPlay controls muted>
-        <source src="assets/videos/demo.mp4" type="video/mp4" />
+        <source src="assets/videos/workflow.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div className="mt-4 rounded-sm shadow-md">
