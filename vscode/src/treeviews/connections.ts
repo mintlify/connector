868c89cedce9ab5a8e41b82465c9709f3ba1a6c6
Han Wang <hyw2@cornell.edu>
2022-07-03 15:08:12 -0400

    List docs in activity bar

--- vscode/src/treeviews/connections.ts
+++ vscode/src/treeviews/connections.ts
@@ -4,6 +4,29 @@ import GlobalState from '../utils/globalState';
 import axios from 'axios';
 import { API_ENDPOINT } from '../utils/api';
 
+type Doc = {
+  _id: string,
+  title: string,
+  lastUpdatedAt: string,
+  createdAt: string,
+  url: string,
+  content: string,
+  code: any[],
+  favicon?: string,
+  method: string,
+  slack?: boolean,
+  email?: boolean,
+};
+
+type Group = {
+  _id: string;
+  name: string;
+  count: number;
+  lastUpdatedDoc: Doc;
+  tasksCount: number;
+  isLoading: boolean;
+};
+
 export class ConnectionsTreeProvider implements vscode.TreeDataProvider<GroupOption> {
   private state: GlobalState;
 
@@ -15,13 +38,24 @@ export class ConnectionsTreeProvider implements vscode.TreeDataProvider<GroupOpt
     return element;
   }
 
-  async getChildren(): Promise<GroupOption[]> {
+  async getChildren(groupElement: GroupOption): Promise<GroupOption[]> {
     const userId = this.state.getUserId();
     if (!userId) {
       return [];
     }
 
     const subdomain = this.state.getSubdomain();
+
+    if (groupElement) {
+      const { data: { docs }  } = await axios.get(`${API_ENDPOINT}/docs/method/${groupElement.group._id}`, {
+        params: {
+          userId,
+          subdomain
+        }
+      });
+      return docs.map((doc) => new DocOption(doc, vscode.TreeItemCollapsibleState.None));
+    }
+
     const { data: { groups }  } = await axios.get(`${API_ENDPOINT}/docs/groups`, {
       params: {
         userId,
@@ -29,9 +63,48 @@ export class ConnectionsTreeProvider implements vscode.TreeDataProvider<GroupOpt
       }
     });
 
-    return groups.map((group) => {
-      return new GroupOption(group.name, group._id, vscode.TreeItemCollapsibleState.None);
-    });
+    return [...groups.map((group) => new GroupOption(group, vscode.TreeItemCollapsibleState.Collapsed)), new AddDocOption()];
+  }
+}
+
+class GroupOption extends vscode.TreeItem {
+  constructor(
+    public readonly group: Group,
+    public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+  ) {
+    super(group.name, collapsibleState);
+    this.tooltip = this.group.name;
+    this.description = `${this.group.count} documents`;
+    this.iconPath = getIconPathForGroup(group._id);
+    // const onClickCommand: vscode.Command = {
+    //   title: 'Hotkey Config',
+    //   command: 'docs.hotkeyConfig',
+    //   arguments: [this.label]
+    // };
+
+    // this.command = onClickCommand;
+  }
+}
+
+class DocOption extends vscode.TreeItem {
+  constructor(
+    public readonly doc: Doc,
+    public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+  ) {
+    super(doc.title, collapsibleState);
+    this.tooltip = this.doc.title;
+    this.iconPath = {
+      light: path.join(__filename, '..', '..', 'assets', 'icons', 'document.svg'),
+      dark: path.join(__filename, '..', '..', 'assets', 'icons', 'document-dark.svg'),
+    };
+  }
+}
+
+class AddDocOption extends vscode.TreeItem {
+  constructor() {
+    super('', vscode.TreeItemCollapsibleState.Collapsed);
+    this.tooltip = '';
+    this.description = 'Add new document';
   }
 }
 
@@ -66,28 +139,3 @@ const getIconPathForGroup = (id: string): string | { light: string, dark: string
       return '';
   }
 };
-
-class GroupOption extends vscode.TreeItem {
-  constructor(
-    public readonly label: string,
-    public readonly id: string,
-    public readonly collapsibleState: vscode.TreeItemCollapsibleState,
-    public readonly isDefault: boolean = false,
-    public readonly selected: boolean = false
-  ) {
-    super(label, collapsibleState);
-    this.tooltip = this.label;
-    if (this.isDefault) {
-      this.description = 'Default';
-    }
-
-    this.iconPath = getIconPathForGroup(id);
-    // const onClickCommand: vscode.Command = {
-    //   title: 'Hotkey Config',
-    //   command: 'docs.hotkeyConfig',
-    //   arguments: [this.label]
-    // };
-
-    // this.command = onClickCommand;
-  }
-}
