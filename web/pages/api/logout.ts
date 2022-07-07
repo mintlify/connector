f8c154a7fc5cd170f0d4511992cc9857b833377d
Han Wang <hyw2@cornell.edu>
2022-07-07 15:19:23 -0400

    Remove web

--- web/pages/api/logout.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiResponse } from "next";
-import { withSession } from "../../lib/withSession";
-
-async function handler(req: any, res: NextApiResponse) {
-  await req.session.destroy();
-  res.redirect('/');
-}
-
-export default withSession(handler);
--- web/pages/api/session.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiResponse } from "next";
-import { withSession } from "../../lib/withSession";
-
-async function handler(req: any, res: NextApiResponse) {
-  const session = req.session.get('user');
-  res.send(session);
-}
-
-export default withSession(handler);
