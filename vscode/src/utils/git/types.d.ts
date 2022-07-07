c9712eb95f06e4ba557e727e5b423afe49761014
Hahnbee Lee <hl985@cornell.edu>
2022-07-07 18:16:39 -0400

    delete duplicated repo interface

--- vscode/src/utils/git/types.d.ts
+++ vscode/src/utils/git/types.d.ts
@@ -403,68 +403,6 @@ export interface BranchQuery {
 	readonly contains?: string;
 }
 
-export interface Repository {
-
-	readonly rootUri: Uri;
-	readonly inputBox: InputBox;
-	readonly state: RepositoryState;
-	readonly ui: RepositoryUIState;
-
-	getConfigs(): Promise<{ key: string; value: string; }[]>;
-	getConfig(key: string): Promise<string>;
-	setConfig(key: string, value: string): Promise<string>;
-	getGlobalConfig(key: string): Promise<string>;
-
-	getObjectDetails(treeish: string, path: string): Promise<{ mode: string, object: string, size: number }>;
-	detectObjectType(object: string): Promise<{ mimetype: string, encoding?: string }>;
-	buffer(ref: string, path: string): Promise<Buffer>;
-	show(ref: string, path: string): Promise<string>;
-	getCommit(ref: string): Promise<Commit>;
-
-	clean(paths: string[]): Promise<void>;
-
-	apply(patch: string, reverse?: boolean): Promise<void>;
-	diff(cached?: boolean): Promise<string>;
-	diffWithHEAD(): Promise<Change[]>;
-	diffWithHEAD(path: string): Promise<string>;
-	diffWith(ref: string): Promise<Change[]>;
-	diffWith(ref: string, path: string): Promise<string>;
-	diffIndexWithHEAD(): Promise<Change[]>;
-	diffIndexWithHEAD(path: string): Promise<string>;
-	diffIndexWith(ref: string): Promise<Change[]>;
-	diffIndexWith(ref: string, path: string): Promise<string>;
-	diffBlobs(object1: string, object2: string): Promise<string>;
-	diffBetween(ref1: string, ref2: string): Promise<Change[]>;
-	diffBetween(ref1: string, ref2: string, path: string): Promise<string>;
-
-	hashObject(data: string): Promise<string>;
-
-	createBranch(name: string, checkout: boolean, ref?: string): Promise<void>;
-	deleteBranch(name: string, force?: boolean): Promise<void>;
-	getBranch(name: string): Promise<Branch>;
-	getBranches(query: BranchQuery): Promise<Ref[]>;
-	setBranchUpstream(name: string, upstream: string): Promise<void>;
-
-	getMergeBase(ref1: string, ref2: string): Promise<string>;
-
-	status(): Promise<void>;
-	checkout(treeish: string): Promise<void>;
-
-	addRemote(name: string, url: string): Promise<void>;
-	removeRemote(name: string): Promise<void>;
-	renameRemote(name: string, newName: string): Promise<void>;
-
-	fetch(options?: FetchOptions): Promise<void>;
-	fetch(remote?: string, ref?: string, depth?: number): Promise<void>;
-	pull(unshallow?: boolean): Promise<void>;
-	push(remoteName?: string, branchName?: string, setUpstream?: boolean, force?: ForcePushMode): Promise<void>;
-
-	blame(path: string): Promise<string>;
-	log(options?: LogOptions): Promise<Commit[]>;
-
-	commit(message: string, opts?: CommitOptions): Promise<void>;
-}
-
 export interface RemoteSource {
 	readonly name: string;
 	readonly description?: string;
