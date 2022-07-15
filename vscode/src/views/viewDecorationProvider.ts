4edd905e50052e41f81425a828ea86c2e543dd5c
Han Wang <hyw2@cornell.edu>
2022-07-14 21:23:40 -0400

    Remove more code

--- vscode/src/views/viewDecorationProvider.ts
+++ vscode/src/views/viewDecorationProvider.ts
@@ -10,7 +10,6 @@ import {
 	window,
 } from 'vscode';
 import { GlyphChars } from '../constants';
-import { GitBranchStatus } from '../git/models';
 
 export class ViewFileDecorationProvider implements FileDecorationProvider, Disposable {
 	private readonly _onDidChange = new EventEmitter<undefined | Uri | Uri[]>();
@@ -59,130 +58,15 @@ export class ViewFileDecorationProvider implements FileDecorationProvider, Dispo
 	}
 
 	provideCommitFileStatusDecoration(uri: Uri, _token: CancellationToken): FileDecoration | undefined {
-		const [, , status] = uri.path.split('/');
-
-		switch (status) {
-			case '!':
-				return {
-					badge: 'I',
-					color: new ThemeColor('gitlens.decorations.ignoredForegroundColor'),
-					tooltip: 'Ignored',
-				};
-			case '?':
-				return {
-					badge: 'U',
-					color: new ThemeColor('gitlens.decorations.untrackedForegroundColor'),
-					tooltip: 'Untracked',
-				};
-			case 'A':
-				return {
-					badge: 'A',
-					color: new ThemeColor('gitlens.decorations.addedForegroundColor'),
-					tooltip: 'Added',
-				};
-			case 'C':
-				return {
-					badge: 'C',
-					color: new ThemeColor('gitlens.decorations.copiedForegroundColor'),
-					tooltip: 'Copied',
-				};
-			case 'D':
-				return {
-					badge: 'D',
-					color: new ThemeColor('gitlens.decorations.deletedForegroundColor'),
-					tooltip: 'Deleted',
-				};
-			case 'M':
-				return {
-					badge: 'M',
-					// color: new ThemeColor('gitlens.decorations.modifiedForegroundColor'),
-					tooltip: 'Modified',
-				};
-			case 'R':
-				return {
-					badge: 'R',
-					color: new ThemeColor('gitlens.decorations.renamedForegroundColor'),
-					tooltip: 'Renamed',
-				};
-			default:
-				return undefined;
-		}
+		return;
 	}
 
 	provideBranchStatusDecoration(uri: Uri, _token: CancellationToken): FileDecoration | undefined {
-		const [, , status] = uri.path.split('/');
-
-		switch (status as GitBranchStatus) {
-			case GitBranchStatus.Ahead:
-				return {
-					badge: '▲',
-					color: new ThemeColor('gitlens.decorations.branchAheadForegroundColor'),
-					tooltip: 'Ahead',
-				};
-			case GitBranchStatus.Behind:
-				return {
-					badge: '▼',
-					color: new ThemeColor('gitlens.decorations.branchBehindForegroundColor'),
-					tooltip: 'Behind',
-				};
-			case GitBranchStatus.Diverged:
-				return {
-					badge: '▼▲',
-					color: new ThemeColor('gitlens.decorations.branchDivergedForegroundColor'),
-					tooltip: 'Diverged',
-				};
-			case GitBranchStatus.MissingUpstream:
-				return {
-					badge: '!',
-					color: new ThemeColor('gitlens.decorations.branchMissingUpstreamForegroundColor'),
-					tooltip: 'Missing Upstream',
-				};
-			case GitBranchStatus.UpToDate:
-				return {
-					badge: '',
-					color: new ThemeColor('gitlens.decorations.branchUpToDateForegroundColor'),
-					tooltip: 'Up to Date',
-				};
-			case GitBranchStatus.Unpublished:
-				return {
-					badge: '▲+',
-					color: new ThemeColor('gitlens.decorations.branchUnpublishedForegroundColor'),
-					tooltip: 'Unpublished',
-				};
-			default:
-				return undefined;
-		}
+		return;
 	}
 
 	provideBranchCurrentDecoration(uri: Uri, _token: CancellationToken): FileDecoration | undefined {
-		const [, , status, current] = uri.path.split('/');
-
-		if (!current) return undefined;
-
-		let color;
-		switch (status as GitBranchStatus) {
-			case GitBranchStatus.Ahead:
-				color = new ThemeColor('gitlens.decorations.branchAheadForegroundColor');
-				break;
-			case GitBranchStatus.Behind:
-				color = new ThemeColor('gitlens.decorations.branchBehindForegroundColor');
-				break;
-			case GitBranchStatus.Diverged:
-				color = new ThemeColor('gitlens.decorations.branchDivergedForegroundColor');
-				break;
-			case GitBranchStatus.UpToDate:
-				color = new ThemeColor('gitlens.decorations.branchUpToDateForegroundColor');
-				break;
-			case GitBranchStatus.Unpublished:
-				color = new ThemeColor('gitlens.decorations.branchUnpublishedForegroundColor');
-				break;
-		}
-
-		return {
-			badge: GlyphChars.Check,
-			color: color,
-			tooltip: 'Current Branch',
-		};
+		return;
 	}
 
 	provideRemoteDefaultDecoration(uri: Uri, _token: CancellationToken): FileDecoration | undefined {
