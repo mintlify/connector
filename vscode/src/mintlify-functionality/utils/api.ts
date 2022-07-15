15be78ec573465271021097418a41ccc7e64319d
Hahnbee Lee <hl985@cornell.edu>
2022-07-14 23:12:07 -0400

    isdev=false

--- vscode/src/mintlify-functionality/utils/api.ts
+++ vscode/src/mintlify-functionality/utils/api.ts
@@ -1,4 +1,4 @@
-export const ISDEV = true;
+export const ISDEV = false;
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const API_ENDPOINT = ISDEV ? 'http://localhost:5000/routes' : 'https://connect.mintlify.com/routes';
